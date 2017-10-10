beea07fe2a4d60d49c12cccfe94564bfbeff72c7
Kazuya Shida <kazuya.4da@gmail.com>
2017-10-10 23:43:02 +0900

    Initialized commit

---
+++ SensorTagCC2650/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SensorTagCC2650
+//
+//  Created by Kazuya Shida on 2017/10/10.
+//  Copyright © 2017 mani3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
